2467ccd023f1c9e1ba5b3624dbc5ef0026a9bd91
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-06-12 16:23:00 +0530

    Clearing tauri file structure and add tailwindcss

---
+++ src/app/main.ts
@@ -0,0 +1,6 @@
+function initBot() {
+  // init bot
+  console.log("Bot initialized successfully!");
+}
+
+export default initBot;
---
+++ src/react/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-700 text-shadow-2xs">Soon to be Hydrion S3lfb0t</h1>
+    </div>
+  );
+}
+
+export default App;
